c06523ffa9d7900c5e995ba5ae9128cd58cd21e1
oramirez2025 <oramirez10102@gmail.com>
2025-11-20 13:51:57 -0500

    Completed second part of school api assignment

--- student_app/models.py
+++ student_app/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
 class Student(models.Model):
-    name = models.CharField(null=False)
-    student_email = models.EmailField(null=False)
-    personal_email = models.EmailField(null=True)
-    locker_number = models.IntegerField(null=False)
-    locker_combination = models.CharField(null=False)
-    good_student = models.BooleanField(null=False)
+    name = models.CharField(null=False, unique=False)
+    student_email = models.EmailField(null=False, unique=True)
+    personal_email = models.EmailField(null=True,unique=True)
+    locker_number = models.IntegerField(null=False,unique=True,default=110)
+    locker_combination = models.CharField(null=False,unique=False,default="12-12-12")
+    good_student = models.BooleanField(null=False,unique=False,default=True)
+    def __str__(self):
+        return f"{self.name} - {self.student_email} - {self.locker_number}"
+    
+    def locker_reassignment(self, new_locker):
+        self.locker_number = new_locker
+    
+    def student_status(self, new_student_value):
+        self.good_student = new_student_value
 
